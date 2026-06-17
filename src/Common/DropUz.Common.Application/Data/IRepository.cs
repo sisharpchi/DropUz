6d27c1a228bc118b7b9c2335b724938e72aec0c8
Ahmadjon Qudratov <qahmadjon11@gmail.com>
2026-06-17 18:06:31 +0500

    fix: repo pattern

--- src/Common/DropUz.Common.Application/Data/IRepository.cs
+++ src/Common/DropUz.Common.Application/Data/IRepository.cs
@@ -1,21 +1,44 @@
+using System.Linq.Expressions;
+
 namespace DropUz.Common.Application.Data;
 
 public interface IRepository : IDisposable
 {
     IUnitOfWork UnitOfWork { get; }
 
-    IQueryable<TEntity> Query<TEntity>()
-        where TEntity : class;
+    Task<int> CountAsync<TEntity>(Expression<Func<TEntity, bool>>? predicate = default)
+           where TEntity : class;
 
-    Task<TEntity?> GetAsync<TEntity>(object? key, CancellationToken cancellationToken = default)
-        where TEntity : class;
+    Task<decimal> SumAsync<TEntity>(Expression<Func<TEntity, decimal>> selector, Expression<Func<TEntity, bool>>? predicate = default)
+            where TEntity : class;
 
-    Task AddAsync<TEntity>(TEntity entity, CancellationToken cancellationToken = default)
-        where TEntity : class;
+    Task<TEntity?> GetAsync<TEntity>(object? id)
+            where TEntity : class;
+
+    Task<TEntity?> GetAsync<TEntity>(Expression<Func<TEntity, bool>> predicate)
+            where TEntity : class;
+
+    Task<List<TEntity>> GetListAsync<TEntity>(Expression<Func<TEntity, bool>>? predicate = default)
+            where TEntity : class;
+
+    Task<Dictionary<TKey, TEntity>> GetDictionaryAsync<TKey, TEntity>(
+            Func<TEntity, TKey> keySelector,
+            Expression<Func<TEntity, bool>>? predicate = default)
+            where TEntity : class
+            where TKey : notnull;
+
+    IQueryable<TEntity> Query<TEntity>(Expression<Func<TEntity, bool>>? predicate = default)
+            where TEntity : class;
+
+    Task AddAsync<TEntity>(TEntity entity)
+            where TEntity : class;
 
     void Add<TEntity>(TEntity entity)
-        where TEntity : class;
+            where TEntity : class;
 
-    void Remove<TEntity>(TEntity entity)
-        where TEntity : class;
+    void Update<TEntity>(TEntity entity)
+            where TEntity : class;
+
+    void Delete<TEntity>(TEntity? entity)
+            where TEntity : class;
 }
--- src/Common/DropUz.Common.Infrastructure/Persistence/MainRepository.cs
+++ src/Common/DropUz.Common.Infrastructure/Persistence/MainRepository.cs
@@ -3,5 +3,7 @@ using DropUz.Common.Infrastructure.Data;
 
 namespace DropUz.Common.Infrastructure.Persistence;
 
-public sealed class MainRepository(MainDbContext context, IUnitOfWork unitOfWork)
-    : Repository(context, unitOfWork), IMainRepository;
+internal class MainRepository(MainDbContext context, IUnitOfWork unitOfWork) : GenericRepository<MainDbContext>(context, unitOfWork), IMainRepository
+{
+}
+
